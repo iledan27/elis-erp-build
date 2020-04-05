c0bb348d106dfa286eec6aebe75c6ce559c91e09
Elena Magda Placinta <magda.placinta@tss-yonder.com>
2020-04-05 16:26:22 +0300

    first build test

---
+++ precache-manifest.9efaa7666fdeb88c43877e72f7e7328f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33bfd6ac8e34a57bbd9d57b0f21a146c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e57348d9301687edfcea",
+    "url": "/static/css/main.ae80000b.chunk.css"
+  },
+  {
+    "revision": "fef14e626345dea2b45d",
+    "url": "/static/js/2.fceca1fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fceca1fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e57348d9301687edfcea",
+    "url": "/static/js/main.bac045fa.chunk.js"
+  },
+  {
+    "revision": "323ddf276ce0f70dd964",
+    "url": "/static/js/runtime-main.3bcf81a4.js"
+  }
+]);
